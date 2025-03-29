d95e6232ddf890d6999c038560371e0bb8a1be0e
jared <jkoontz09@gmail.com>
2025-03-29 00:53:19 -0600

    review comments

--- yt_feed/utils/batch_helper.py
+++ yt_feed/utils/batch_helper.py
@@ -5,9 +5,9 @@ from yt_feed.models.data_entries import parse_video_id
 _BATCH_SIZE = 50
 
 
-def batch_yt_calls(playlist_data):
+def batch_iter(playlist_data):
     """
-    youtube uses GET for all of these calls, so we are bounded by the url limit.
+    YouTube uses GET for all of these calls, so we are bounded by the url limit.
     If we have a lot of video ids, we need to batch them, 50 at a time seems reasonable.
     """
     video_ids = [parse_video_id(video_id) for video_id in playlist_data]
--- yt_feed/utils/render_feed.py
+++ yt_feed/utils/render_feed.py
@@ -3,7 +3,7 @@ import datetime
 from flask import render_template
 
 from yt_feed.models.data_entries import ChannelEntry
-from yt_feed.utils.batch_calls import batch_yt_calls
+from yt_feed.utils.batch_helper import batch_iter
 from yt_feed.utils.env_vars import domain
 from yt_feed.utils.youtube_api_call import yt_videos
 
@@ -12,7 +12,7 @@ def render_rss_feed(
     playlist_data: list[dict], channel_data: ChannelEntry, podcast_type: str
 ):
     videos_data = []
-    for yt_id_batch in batch_yt_calls(playlist_data):
+    for yt_id_batch in batch_iter(playlist_data):
         videos_data.extend(yt_videos(yt_id_batch))
 
     all_data = {
