56a324919c5f87a3e526931c7e9dac7ff33128ed
jared <jkoontz09@gmail.com>
2025-03-31 14:01:38 -0600

    making tests pass in ci

--- test/conftest.py
+++ test/conftest.py
@@ -1,10 +1,18 @@
-import pytest
+import os
 
-from yt_feed.web_app import yt_feed_app
+import pytest
 
 
 @pytest.fixture()
-def app():
+def app(monkeypatch):
+    envs = {
+        "DOMAIN": "http://0.0.0.0",
+        "YOUTUBE_API_KEY": "deadbeef",
+    }
+    monkeypatch.setattr(os, "environ", envs)
+
+    from yt_feed.web_app import yt_feed_app
+
     app = yt_feed_app
     app.config.update(
         {
