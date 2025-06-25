d10edec257b7846b1ad7c8d355c785646a71f20d
Jared Koontz <jkoontz09@gmail.com>
2025-06-24 19:55:37 -0600

    docs: video and audio endpoints

--- wsgi.py
+++ wsgi.py
@@ -1,10 +1,8 @@
 from yt_feed.web_app import yt_feed_app
 
-
 """
 For local dev only. For prod you should be using gunicorn
 """
-
 
 if __name__ == "__main__":
     yt_feed_app.run(host="0.0.0.0", port=5446)
