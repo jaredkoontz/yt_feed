d0f4921209d94ca011efa0cb190f5338c2c32cef
Jared Koontz <jkoontz09@gmail.com>
2025-07-01 20:48:22 -0600

    chore: updating paths

--- yt_feed/utils/render_feed.py
+++ yt_feed/utils/render_feed.py
@@ -8,9 +8,7 @@ from yt_feed.utils.env_vars import domain
 from yt_feed.utils.yt_api_wrapper import yt_videos
 
 
-def render_rss_feed(
-    playlist_data: list[dict], channel_data: ChannelEntry, podcast_type: str
-):
+def render_rss_feed(playlist_data: list[dict], channel_data: ChannelEntry):
     videos_data = []
     for yt_id_batch in batch_iter(playlist_data):
         videos_data.extend(yt_videos(yt_id_batch))
@@ -18,7 +16,7 @@ def render_rss_feed(
     all_data = {
         "video_info": videos_data,
         "channel_data": channel_data,
-        "podcast_type": podcast_type,
+        "podcast_type": "audio",
         "media_extension": "m4a",
     }
     return render_template(
