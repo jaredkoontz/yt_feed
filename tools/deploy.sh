c2ff70747d5b05279c21955b52530b47200d3e98
jared <jkoontz09@gmail.com>
2025-08-06 16:22:43 -0600

    feat: sample deployment script (#35)

---
+++ tools/deploy.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# this is a template script for deploying.
+# if you want to make changes to this, you could `git update-index --assume-unchanged <filename>`
+
+YT_KEY=""
+DOM=""
+
+set -x
+
+docker pull ghcr.io/jaredkoontz/yt_feed:main
+
+docker stop $(docker ps -a -q)
+
+docker run\
+ -e YOUTUBE_API_KEY="$YT_KEY"\
+ -e DOMAIN="$DOM"\
+ -p 80:80\
+ --rm -d\
+ ghcr.io/jaredkoontz/yt_feed:main
