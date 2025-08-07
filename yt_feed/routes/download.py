e1f3cbf745b5346e6957297247e190c073355e57
jared <jkoontz09@gmail.com>
2025-08-06 18:49:58 -0600

    refactor: make explicit which routes generate xml (#39)

--- yt_feed/routes/download.py
+++ yt_feed/routes/download.py
@@ -4,11 +4,10 @@ from flask import Response
 
 from yt_feed.utils.ytdlp_inter import extract_audio
 
-download_page = Blueprint("download_page", __name__)
+download_route = Blueprint("download_page", __name__)
 
 
-@download_page.route("/dl/<video_id>.<suffix>")
+@download_route.route("/dl/<video_id>.<suffix>")
 def yt_dl(video_id: str, suffix: str) -> Response:
     result = extract_audio(video_id)
-    r = result["url"]
-    return redirect(r)
+    return redirect(result["url"])
--- yt_feed/web_app.py
+++ yt_feed/web_app.py
@@ -1,17 +1,17 @@
 from flask import Flask
 
-from yt_feed.routes.channel import channel_page
-from yt_feed.routes.download import download_page
+from yt_feed.routes.download import download_route
 from yt_feed.routes.index import index_page
-from yt_feed.routes.playlist import playlist_page
-from yt_feed.routes.yt_user import user_page
+from yt_feed.routes.rss.channel import channel_page
+from yt_feed.routes.rss.playlist import playlist_page
+from yt_feed.routes.rss.yt_user import user_page
 from yt_feed.utils.channel_cache import cache_config
 from yt_feed.utils.channel_cache import flask_cache
 
 yt_feed_app = Flask(__name__, template_folder="templates")
 
 yt_feed_app.register_blueprint(channel_page)
-yt_feed_app.register_blueprint(download_page)
+yt_feed_app.register_blueprint(download_route)
 yt_feed_app.register_blueprint(index_page)
 yt_feed_app.register_blueprint(playlist_page)
 yt_feed_app.register_blueprint(user_page)
