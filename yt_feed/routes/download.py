75c186ac48d8858e011b0a293140022e111c1f72
Jared Koontz <jkoontz09@gmail.com>
2025-12-31 11:40:06 -0700

    feat: address #32. No longer fail when encountering age inappropriate content error.

--- yt_feed/routes/download.py
+++ yt_feed/routes/download.py
@@ -1,6 +1,8 @@
 from flask import Blueprint
+from flask import make_response
 from flask import redirect
 from flask import Response
+from yt_dlp.utils import DownloadError
 
 from yt_feed.utils.ytdlp_inter import extract_audio
 
@@ -9,5 +11,18 @@ download_route = Blueprint("download_page", __name__)
 
 @download_route.route("/dl/<video_id>.<suffix>")
 def yt_dl(video_id: str, suffix: str) -> Response:
-    result = extract_audio(video_id)
+    try:
+        result = extract_audio(video_id)
+    except DownloadError as e:
+        # unfortunately, yt_dlp does not provide a way to get the error type. It is just a generic DownloadError
+        # so we have to do string parsing.
+        err_msg = e.msg
+        if "Sign in to confirm your age." in err_msg:
+            return make_response(
+                "Youtube has flagged this as age inappropriate content and we do not support cookies",
+                501,
+            )
+        else:
+            return make_response(f"Error downloading video: {err_msg}", 500)
+
     return redirect(result["url"])
